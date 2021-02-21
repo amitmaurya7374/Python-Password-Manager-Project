f077dbf1c48297c2031e819b3f6b11161ed3d395
amitmaurya7374 <amitmaurya7374@gmail.com>
2021-02-21 10:51:21 +0530

    create a window with image

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+# creating a window
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+canvas = Canvas(height=400, width=400, highlightthickness=0)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(200, 200, image=logo_img)
+canvas.grid(row=1, column=1)
+
+window.mainloop()
